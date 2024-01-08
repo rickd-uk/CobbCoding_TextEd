24e481bf8f897773708699a564635add6fefdfca
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-08 18:58:36 +0900

    add: prints a msg if F1 pressed

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 #include <stdio.h>
 
 int main(void) {
+  // initializes the library
   initscr();
-  printw("Hello world\n");
+  raw();
+  keypad(stdscr, TRUE);
+  noecho();
+
+  int ch = getch();
+
+  if (ch == KEY_F(1)) {
+    printw("F1 key pressed!\n");
+  }
+
   refresh();
   getch();
   endwin();
