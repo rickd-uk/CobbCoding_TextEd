d70db6ad8207d0a32078dbe9b682dab44c803cda
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-13 22:54:03 +0900

    update: ctrl + q now exits

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <curses.h>
 #include <stdio.h>
 
+#define ctrl(x) ((x) & 0x1f)
+
 int main(void) {
   // initializes the library
   initscr();
@@ -17,7 +19,7 @@ int main(void) {
   int ch = getch();
   addch(ch);
 
-  while (ch != 'q') {
+  while (ch != ctrl('q')) {
     ch = getch();
 
     if (ch == 263) {
@@ -25,14 +27,15 @@ int main(void) {
       getyx(stdscr, y, x);
       move(y, x - 1);
       delch();
+    } else if (ch == ctrl('q')) {
+      printw("CTRL + q\n");
+      break;
     } else {
       addch(ch);
     }
-    /* printw("%d", ch); */
   }
-
   refresh();
-  /* getch(); */
+
   endwin();
 
   return 0;
