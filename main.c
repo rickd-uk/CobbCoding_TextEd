f2ca1f5fda220dca7e8fa36f29d323ad53ea337e
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-13 23:37:11 +0900

    add: defined enum for mode; BACKSPACE in place of 263

--- main.c
+++ main.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 
 #define ctrl(x) ((x) & 0x1f)
+#define BACKSPACE 263
+
+typedef enum { NORMAL, INSERT } Mode;
+
+Mode mode = NORMAL;
+
+char *stringify_mode() {
+  switch (mode) {
+    case NORMAL:
+      return "NORMAL";
+      break;
+    case INSERT:
+      return "INSERT";
+      break;
+  }
+  return "NORMAL";
+}
 
 int main(void) {
   // initializes the library
@@ -13,26 +30,27 @@ int main(void) {
   int row, col;
   getmaxyx(stdscr, row, col);
 
-  mvprintw(row - 1, 0, "NORMAL");
+  mvprintw(row - 1, 0, stringify_mode());
   move(0, 0);
 
   int ch = getch();
   addch(ch);
 
+  int x, y = 0;
+  getyx(stdscr, y, x);
   while (ch != ctrl('q')) {
+    mvprintw(row - 1, 0, stringify_mode());
+    move(y, x);
     ch = getch();
 
-    if (ch == 263) {
-      int x, y;
+    if (ch == BACKSPACE) {
       getyx(stdscr, y, x);
       move(y, x - 1);
       delch();
-    } else if (ch == ctrl('q')) {
-      printw("CTRL + q\n");
-      break;
     } else {
       addch(ch);
     }
+    getyx(stdscr, y, x);
   }
   refresh();
 
