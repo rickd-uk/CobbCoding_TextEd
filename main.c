7083220e097805035b3b11de6b4f3b8d8551d353
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-14 16:24:00 +0900

    add: requires INSERT mode to enter text;  esc returns to NORMAL mode

--- main.c
+++ main.c
@@ -3,18 +3,22 @@
 
 #define ctrl(x) ((x) & 0x1f)
 #define BACKSPACE 263
+#define ESCAPE 27
 
 typedef enum { NORMAL, INSERT } Mode;
 
 Mode mode = NORMAL;
 
-char *stringify_mode() {
+const char *stringify_mode() {
   switch (mode) {
     case NORMAL:
       return "NORMAL";
       break;
     case INSERT:
       return "INSERT";
+      break;
+    default:
+      return "NORMAL";
       break;
   }
   return "NORMAL";
@@ -30,25 +34,34 @@ int main(void) {
   int row, col;
   getmaxyx(stdscr, row, col);
 
-  mvprintw(row - 1, 0, stringify_mode());
+  mvprintw(row - 1, 0, "%s", stringify_mode());
   move(0, 0);
 
-  int ch = getch();
-  addch(ch);
+  int ch;
 
   int x, y = 0;
-  getyx(stdscr, y, x);
   while (ch != ctrl('q')) {
-    mvprintw(row - 1, 0, stringify_mode());
+    mvprintw(row - 1, 0, "%s", stringify_mode());
     move(y, x);
     ch = getch();
 
-    if (ch == BACKSPACE) {
-      getyx(stdscr, y, x);
-      move(y, x - 1);
-      delch();
-    } else {
-      addch(ch);
+    switch (mode) {
+      case NORMAL:
+        if (ch == 'i') {
+          mode = INSERT;
+        }
+        break;
+      case INSERT:
+        if (ch == BACKSPACE) {
+          getyx(stdscr, y, x);
+          move(y, x - 1);
+          delch();
+        } else if (ch == ESCAPE) {
+          mode = NORMAL;
+        } else {
+          addch(ch);
+        }
+        break;
     }
     getyx(stdscr, y, x);
   }
